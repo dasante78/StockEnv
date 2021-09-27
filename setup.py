745bee0c73972c100b10fa163de9ce00c17d8f4e
Daniel Prakah-Asante <doprakah@gmail.com>
2021-09-27 14:48:53 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,30 @@
 from setuptools import setup, find_packages
 import os
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
-VERSION = '0.0.1'
+VERSION = '0.1.1'
 DESCRIPTION = 'Stock environment for training machine learning agents'
 
 # Setting up
 setup(
-    name="StockEnv",
+    name="stocktrainer",
     version=VERSION,
     author="Daniel Prakah-Asante",
     author_email="doprakah@mit.edu",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    long_description=long_description,
+     packages=find_packages(),
+     url='https://github.com/dasante78/StockTrainer',  
      keywords = ['USEFULL', 'STOCKS','MACHINE LEARNING', 'AI', 'ARTIFICAL INTELLIGENCE' ],   
   install_requires=[           
           'pandas',
           'numpy',
           'datetime',
-          'pandas_datareader'
+          'pandas_datareader',
+          'yfinance'
       ],
         classifiers=[
     'Development Status :: 3 - Alpha',      
